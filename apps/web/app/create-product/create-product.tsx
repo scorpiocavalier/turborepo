61a5a0b7b04a0ecd626c581a6c589286321488d8
Arturo Chen <art.can.fly@gmail.com>
2025-02-11 23:03:02 -0500

    add simple form to create product

---
+++ apps/web/app/create-product/create-product.tsx
@@ -0,0 +1,19 @@
+export default async function CreateProduct() {
+  return (
+    <div>
+      <form action="">
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" name="name" id="name" />
+        </div>
+
+        <div>
+          <label htmlFor="price">Price:</label>
+          <input type="number" name="price" id="price" />
+        </div>
+
+        <button type="submit">Create Product</button>
+      </form>
+    </div>
+  );
+}
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,5 @@
+import CreateProduct from "./create-product/create-product";
+
 export default function Home() {
-  return <></>;
+  return <CreateProduct />;
 }
