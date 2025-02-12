7888672b5842b6a777c201bceb4e8bfaa9828ce9
Arturo Chen <art.can.fly@gmail.com>
2025-02-11 23:13:59 -0500

    add a createProduct server action

---
+++ apps/web/app/create-product/actions/create-product.ts
@@ -0,0 +1,9 @@
+"use server";
+
+export default async function createProduct(formData: FormData) {
+  await fetch(`${process.env.API_URL}/products`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(Object.fromEntries(formData))
+  });
+}
--- apps/web/app/create-product/create-product.tsx
+++ apps/web/app/create-product/create-product.tsx
@@ -1,7 +1,9 @@
+import createProduct from "./actions/create-product";
+
 export default async function CreateProduct() {
   return (
     <div>
-      <form action="">
+      <form action={createProduct}>
         <div>
           <label htmlFor="name">Name:</label>
           <input type="text" name="name" id="name" />
